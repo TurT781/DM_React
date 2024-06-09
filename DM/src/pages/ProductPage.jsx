ebd7f91ecefa739ed87b5f082e203e40b034479d
Flavien Ptrk <flavien.patriarca2002@gmail.com>
2024-06-09 22:15:25 +0200

    fin

--- DM/src/pages/ProductPage.jsx
+++ DM/src/pages/ProductPage.jsx
@@ -4,10 +4,10 @@ import Products from '../components/Products';
 
 const ProductPage = () => {
     return (
-        <div>
+        <div className='allProduct'>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
-            <h1>Livres Disponibles </h1>
+            <h1 className='H1product'>Livres Disponibles </h1>
             <Products />
         </div>
     );
